426b8f2859855224c9375ca5589680322633296e
CholponaiNaimanova <naimanovacholpon@gmail.com>
2024-07-22 16:35:08 +0600

    events-page

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,13 @@ import "./App.scss";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import { Route, Routes } from "react-router-dom";
-import Meropriatia from "./components/Meropriatia";
 import Videoo from "./components/Videoo";
 import Organizasii from "./components/Organizasii";
 import Soobshestvo from "./components/Soobshestvo";
 import DetailVacancy from "./components/DetailVacancy";
 import { Vacancies } from "./components/Vacancy/Vacancies";
 import DetailOrganization from "./components/DetailOrganization";
+import Events from "./components/Events";
 
 function App() {
   return (
@@ -16,7 +16,7 @@ function App() {
       <Header />
       <Routes>
         <Route path="/vacancy" element={<Vacancies />} />
-        <Route path="/events" element={<Meropriatia />} />
+        <Route path="/events" element={<Events />} />
         <Route path="/meetups" element={<Videoo />} />
         <Route path="/organizations" element={<Organizasii />} />
         <Route path="/community" element={<Soobshestvo />} />
---
+++ src/components/Events/index.tsx
@@ -0,0 +1,72 @@
+import React, { useEffect, useState } from "react";
+import useFetch from "../../hooks/useFetch";
+import EventsList from "./EventsList";
+
+
+function Events() {
+const events_url = 'http://3.38.98.134/events' 
+  
+const useFetch = ({
+  url
+} = {
+  url: events_url,
+}) => {
+  const [data, setData] = useState<any>([])
+  const [loading, setLoading] = useState(false)
+
+  const fetchData = async () => {
+      setLoading(true)
+      try{
+          const response = await fetch(url)
+          const data = await response.json()
+          if(data.statusCode === 200) {
+              setData(data.data);
+          }
+      } catch(error) {
+          console.log(error);
+      }
+      finally{
+          setLoading(false)
+      }
+  }
+
+  useEffect(() =>{
+      fetchData()
+  }, [])
+
+  return {data, loading}
+}
+
+const  { data, loading } = useFetch();
+console.log(data, 'events');
+
+if (loading) {
+  return <div>Loading</div>;
+}
+ 
+
+  return (
+    <div id="events">
+      <div className="container">
+        <div className="btn1">
+          <button>Добавить мероприятиe</button>
+        </div>
+        {data &&
+                  data.map((el: any, index: number) => {
+                    return (
+                      <EventsList
+                        key={index}
+                        location={el.location}
+                        name={el.name}
+                        organization_name={el.organization_name}
+                        date={el.date}
+                        
+                      />
+                    );
+                  })}
+      </div>
+    </div>
+  )
+}
+
+export default Events
