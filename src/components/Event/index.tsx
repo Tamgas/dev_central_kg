e08ee6a3854f98f95ecdd23de5aa3ce59dfb5601
Aidaidev <bakytbekovnaa0100@gmail.com>
2024-07-24 02:44:58 +0600

    events -integration

---
+++ src/components/Event/index.tsx
@@ -0,0 +1,37 @@
+import useFetch from "../../hooks/useFetch";
+import EventList from "./EventList";
+
+function Events() {
+  const events_url = "http://3.38.98.134/events";
+  const { data, loading } = useFetch({
+    url: events_url,
+  });
+
+  if (loading) {
+    return <div className="loading">Loading...</div>;
+  }
+  return (
+    <div id="events">
+      <div className="container">
+        <div className="btn1">
+          <button>Добавить мероприятиe</button>
+        </div>
+        {data &&
+          data.map((el: any, index: number) => {
+            return (
+              <EventList
+                key={index}
+                cover={el.cover_formats}
+                location={el.location}
+                name={el.name}
+                organization_name={el.organization_name}
+                date={el.date}
+              />
+            );
+          })}
+      </div>
+    </div>
+  );
+}
+
+export default Events;
