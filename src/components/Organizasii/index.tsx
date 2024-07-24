10e209b8a2f9fdc9952e96beb2e7a5ec1f6a8013
Aidaidev <bakytbekovnaa0100@gmail.com>
2024-07-25 00:31:40 +0600

    organizations-integration

--- src/components/Organizasii/index.tsx
+++ src/components/Organizasii/index.tsx
@@ -1,39 +1,35 @@
 import React from "react";
-import { useNavigate } from "react-router-dom";
+import OrganizationList from "./OrganizationList";
+import useFetch from "../../hooks/useFetch";
 
 function Organizasii() {
-  const nav = useNavigate();
+  const { data, loading } = useFetch({
+    url: "http://3.38.98.134/organizations",
+  });
+
+  if (loading) {
+    return <div className="loading">Loading...</div>;
+  }
+  console.log(data);
+
   return (
     <div id="organizasii">
       <div className="container">
         <div className="organizasii">
           <div className="organizasii--cards">
-            <div className="organizasii--cards__card">
-              <div className="organizasii--cards__card--title">
-                <img
-                  src="https://devkg.com/images/organizations/88ba3e55ab38d5fe5c710cb8aede1e6f.webp"
-                  alt="img"
-                />
-                <div className="organizasii--cards__card--title__company">
-                  <h3>Компания</h3>
-                  <h2 onClick={() => nav("/detailO")}>MBank</h2>
-                </div>
-              </div>
-              <div className="organizasii--cards__card--about">
-                <div className="organizasii--cards__card--about__block">
-                  <h3>Вакансии</h3>
-                  <h2>166</h2>
-                </div>
-                <div className="organizasii--cards__card--about__block">
-                  <h3>Мероприятия</h3>
-                  <h2>1</h2>
-                </div>
-                <div className="organizasii--cards__card--about__block">
-                  <h3>Видео</h3>
-                  <h2>0</h2>
-                </div>
-              </div>
-            </div>
+            {data &&
+              data.map((el: any, index: number) => {
+                return (
+                  <OrganizationList
+                    icon={el.icon_formats}
+                    key={index}
+                    name={el.name}
+                    jobs_count={el.jobs_count}
+                    meetups_count={el.meetups_count}
+                    events_count={el.events_count}
+                  />
+                );
+              })}
           </div>
           <div className="organizasii--btn">
             <button>Следующая страница</button>
